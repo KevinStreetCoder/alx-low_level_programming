507bf7a6b4be48752b58ff73f86878fdf9f02f69
KevinStreetCoder <kevinisaackareithi@gmail.com>
2023-03-17 19:11:02 +0300

    13th commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,27 +7,27 @@
  */
 int main(void)
 {
-    int i, j;
+int i, j;
 
-    for (i = 0; i < 100; i++)
-    {
-        for (j = i + 1; j < 100; j++)
-        {
-            putchar(i / 10 + '0');
-            putchar(i % 10 + '0');
-            putchar(' ');
-            putchar(j / 10 + '0');
-            putchar(j % 10 + '0');
+for (i = 0; i < 100; i++)
+{
+for (j = i + 1; j < 100; j++)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
 
-            if (i == 98 && j == 99)
-                continue;
+if (i == 98 && j == 99)
+continue;
 
-            putchar(',');
-            putchar(' ');
-        }
-    }
+putchar(',');
+putchar(' ');
+}
+}
 
-    putchar('\n');
+putchar('\n');
 
-    return (0);
+return (0);
 }
