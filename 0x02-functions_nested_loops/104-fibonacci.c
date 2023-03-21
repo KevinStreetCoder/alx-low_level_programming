4e8b9d300f413d96f7dac7acdc0ecda4db4c3e31
KevinStreetCoder <kevinisaackareithi@gmail.com>
2023-03-21 19:39:12 +0300

    17th commit functions and loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,20 +8,72 @@
  */
 int main(void)
 {
-	int i;
-	unsigned long int num1 = 1, num2 = 2, sum;
+	int inc;
 
-	printf("%lu, %lu", num1, num2);
+	unsigned long n1 = 0, n2 = 1, n3;
 
-	for (i = 0; i < 96; i++)
+	unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
+
+	unsigned long h1, h2;
+
+
+
+	for (inc = 0; inc < 92; inc++)
+
 	{
-		sum = num1 + num2;
-		printf(", %lu", sum);
-		num1 = num2;
-		num2 = sum;
+
+		n3 = n1 + n2;
+
+		printf("%lu, ", n3);
+
+		n1 = n2;
+
+		n2 = n3;
+
 	}
 
-	printf("\n");
+	n1_h1 = n1 / 10000000000;
 
-	return (0);
+	n2_h1 = n2 / 10000000000;
+
+	n1_h2 = n1 % 10000000000;
+
+	n2_h2 = n2 % 10000000000;
+
+	for (inc = 93; inc < 99; inc++)
+
+	{
+
+		h1 = n1_h1 + n2_h1;
+
+		h2 = n1_h2 + n2_h2;
+
+		if ((n1_h2 + n2_h2) > 9999999999)
+
+		{
+
+
+
+			h1 += 1;
+
+			h2 %= 10000000000;
+
+		}
+
+		printf("%lu%lu", h1, h2);
+
+		if (inc != 98)
+
+			printf(", ");
+
+
+
+		n1_h1 = n2_h1;
+
+		n1_h2 = n2_h2;
+
+		n2_h1 = h1;
+
+		n2_h2 = h2;
 	}
+}
