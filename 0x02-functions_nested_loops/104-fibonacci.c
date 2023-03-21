5fd5cfc96918892839d06f8c0186e6af714d7d8e
KevinStreetCoder <kevinisaackareithi@gmail.com>
2023-03-21 19:42:43 +0300

    17th commit functions and loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,80 +1,41 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - Prints the first 98 Fibonacci numbers, starting with 1 and 2,
- *        followed by a new line.
+ * main - first 50 fibonatchi.
  *
- * Return: Always 0.
+ * Return: void.
  */
 int main(void)
 {
+	unsigned long int i, i0, i1, j, j0, j1, e, e0, e1, y, k;
 
-	int inc;
+	i = 1;
+	j = 2;
+	e = i + j;
 
-	unsigned long n1 = 0, n2 = 1, n3;
-
-	unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
-
-	unsigned long h1, h2;
-
-
-
-	for (inc = 0; inc < 92; inc++)
-
+	printf("%lu, ", i);
+	printf("%lu, ", j);
+	for (y = 3; y < 89; y++)
 	{
-
-		n3 = n1 + n2;
-
-		printf("%lu, ", n3);
-
-		n1 = n2;
-
-		n2 = n3;
-
+		printf("%lu, ", e);
+		i = j;
+		j = e;
+		e = i + j;
 	}
-
-	n1_h1 = n1 / 10000000000;
-
-	n2_h1 = n2 / 10000000000;
-
-	n1_h2 = n1 % 10000000000;
-
-	n2_h2 = n2 % 10000000000;
-
-	for (inc = 93; inc < 99; inc++)
-
+	j0 = j / 1000000000;
+	j1 = j % 1000000000;
+	e0 = e / 1000000000;
+	e1 = e % 1000000000;
+	for (k = 89; k < 98; k++)
 	{
-
-		h1 = n1_h1 + n2_h1;
-
-		h2 = n1_h2 + n2_h2;
-
-		if ((n1_h2 + n2_h2) > 9999999999)
-
-		{
-
-
-
-			h1 += 1;
-
-			h2 %= 10000000000;
-
-		}
-
-		printf("%lu%lu", h1, h2);
-
-		if (inc != 98)
-
-			printf(", ");
-
-
-
-		n1_h1 = n2_h1;
-
-		n1_h2 = n2_h2;
-
-		n2_h1 = h1;
-
-		n2_h2 = h2;
-
+		printf("%lu%lu, ", e0, e1);
+		i0 = j0;
+		i1 = j1;
+		j0 = e0;
+		j1 = e1;
+		e0 = i0 + j0 + ((i1 + j1) / 1000000000);
+		e1 = (i1 + j1) % 1000000000;
 	}
+printf("%lu%lu\n", e0, e1);
+	return (0);
+}
