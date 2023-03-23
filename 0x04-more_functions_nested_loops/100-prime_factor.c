334a42372fe42211873edd8e4e1dba3d065fb13d
KevinStreetCoder <kevinisaackareithi@gmail.com>
2023-03-23 13:10:41 +0300

    Commits for more_function_nested_loops tasks

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the largest prime factor of the number 612852475143
+ * main - finds and prints the largest prime
+ * factor of the number 612852475143
  *
  * Return: Always 0
  */
 int main(void)
 {
-    long int n = 612852475143;
-    long int factor = 2;
-    long int largest_factor;
+	long int n = 612852475143;
+	long int factor = 2;
+	long int largest_factor;
 
-    while (n != 0)
-    {
-        if (n % factor != 0)
-            factor++;
-        else
-        {
-            largest_factor = n;
-            n /= factor;
-            if (n == 1)
-            {
-                printf("%ld\n", largest_factor);
-                break;
-            }
-        }
-    }
-    return (0);
+	while (n != 0)
+	{
+		if (n % factor != 0)
+			factor++;
+		else
+		{
+			largest_factor = n;
+			n /= factor;
+			if (n == 1)
+			{
+				printf("%ld\n", largest_factor);
+				break;
+			}
+		}
+	}
+	return (0);
 }
