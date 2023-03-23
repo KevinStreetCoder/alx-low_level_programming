872aeafd138f057c6e478239c4e07dd0a75d9ee3
KevinStreetCoder <kevinisaackareithi@gmail.com>
2023-03-23 13:19:44 +0300

    Commits for more_function_nested_loops tasks

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
- *
- * This function causes an infinite loop and then avoids it
- *
- * Return: Always 0 (Success)
+ * main - causes an infinite loop
+ * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -18,7 +16,7 @@ int main(void)
 	/*while (i < 10)*/
 	{
 		/*putchar(i);*/
-	} 
+	}
 
 	printf("Infinite loop avoided! \\o/\n");
 
