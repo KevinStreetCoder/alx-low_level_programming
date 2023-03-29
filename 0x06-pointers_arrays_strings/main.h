134cd9a2e02c43f713b51b58c35b61c94f565bc0
KevinStreetCoder <kevinisaackareithi@gmail.com>
2023-03-29 21:03:02 +0300

    Pointers Array String Commits

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -29,6 +29,10 @@ char *cap_string(char *);
 char *leet(char *);
 
 /* task 8 */
+char *rot13(char *);
+
 void print_number(int n);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif /* MAIN_H */
