9d1eb32edbe38de72fdb083bccdb8c5161de9e82
KevinStreetCoder <kevinisaackareithi@gmail.com>
2023-04-03 20:42:12 +0300

    more pointers array string commits

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 /* Function prototypes */
 char *_memset(char *s, char b, unsigned int n);
-void *_memcpy(void *dest, const void *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
