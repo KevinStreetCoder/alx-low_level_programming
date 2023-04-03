68ce5a9b2943d40c373847c5cea12ce933e8fb79
KevinStreetCoder <kevinisaackareithi@gmail.com>
2023-04-03 21:16:57 +0300

    more pointers array string commits

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,6 +10,7 @@ void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
+void set_string(char **s, char *to);
 
 /* Helper function prototype */
 int _putchar(char c);
