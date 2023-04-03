c89cd9203e6ff8df52f9b6058b826a1ee1ca4f87
KevinStreetCoder <kevinisaackareithi@gmail.com>
2023-04-03 20:53:06 +0300

    more pointers array string commits

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,18 +10,22 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	char *bhaystack;
+	char *pneedle;
 
-	for (i = 0; haystack[i]; i++)
+	while (*haystack != '\0')
 	{
-		for (j = 0; needle[j]; j++)
-		{
-			if (haystack[i + j] != needle[j])
-				break;
-		}
-		if (!needle[j])
-			return (haystack + i);
-	}
+		bhaystack = haystack;
+		pneedle = needle;
 
-	return (NULL);
+		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
+		{
+			haystack++;
+			pneedle++;
+		}
+		if (!*pneedle)
+			return (bhaystack);
+		haystack = bhaystack + 1;
+	}
+	return (0);
 }
