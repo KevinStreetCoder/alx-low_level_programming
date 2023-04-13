fa4b73980da76d9592f04a2d317b24466a66594c
KevinStreetCoder <kevinisaackareithi@gmail.com>
2023-04-14 01:25:14 +0300

    more malloc commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,17 +9,22 @@
  */
 int *array_range(int min, int max)
 {
-	int *arr, i;
+	int *b, i = 0;
 
 	if (min > max)
 		return (NULL);
 
-	arr = malloc((max - min + 1) * sizeof(int));
-	if (arr == NULL)
+	b = malloc((sizeof(int) * (max - min)) + sizeof(int));
+
+	if (b == NULL)
 		return (NULL);
 
-	for (i = 0; i < (max - min + 1); i++)
-		arr[i] = min + i;
+	while (min <= max)
+	{
+		b[i] = min;
+		i++;
+		min++;
+	}
 
-	return (arr);
+	return (b);
 }
