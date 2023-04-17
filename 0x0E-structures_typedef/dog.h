7ae8a712d1ece04fb1348b9d4b200ddc5fdc5955
KevinStreetCoder <kevinisaackareithi@gmail.com>
2023-04-17 20:27:02 +0300

    structure in c commits

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct representing a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: a struct representing a dog
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+} dog_t;
+
+/* Function prototypes */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif /* DOG_H */
