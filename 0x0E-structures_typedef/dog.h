6c176734f6093f2d4d6daa130f25a3c990db1146
KevinStreetCoder <kevinisaackareithi@gmail.com>
2023-04-17 20:46:12 +0300

    structure in c commits

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,5 +22,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 /* function that prints a struct dog */
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
