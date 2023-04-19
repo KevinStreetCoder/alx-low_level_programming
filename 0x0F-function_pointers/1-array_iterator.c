dc4e2e763c5f9e59e74f9d9012c88d23120b09e1
KevinStreetCoder <kevinisaackareithi@gmail.com>
2023-04-19 20:30:01 +0300

    Function pointers commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 
 /**
- * array_iterator - execute a function on each element of an array
+ * array_iterator - execute a function
+ * on each element of an array
  * @array: the array to iterate over
  * @size: the size of the array
- * @action: a pointer to the function to execute on each element
+ * @action: a pointer to the function to execute
+ * on each element
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
