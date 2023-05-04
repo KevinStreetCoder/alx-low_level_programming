72052ea24bee053146056ed0b4e0eaf7dfb6c639
KevinStreetCoder <kevinisaackareithi@gmail.com>
2023-05-04 21:17:45 +0300

    Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,20 +10,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
+        unsigned int result = 0;
 
-	if (b == NULL)
-		return (0);
+        if (b == NULL)
+                return (0);
 
-	while (*b)
-	{
-		if (*b != '0' && *b != '1')
-			return (0);
+        for (int i = 0; b[i] != '\0'; i++)
+        {
+                if (b[i] != '0' && b[i] != '1')
+                        return (0);
 
-		num <<= 1;
-		if (*b == '1')
-			num += 1;
-		b++;
-	}
-	return (num);
+                result = result * 2 + (b[i] - '0');
+        }
+
+        return (result);
 }
